ad3f1a30c7c4a11dafb323570c563c618430a901
Niklas Persson <n.persson@cern.ch>
2019-09-11 16:13:50 +0200

    global: fix travis errors

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,10 @@
 # Ignoring false positive 36759 (reporting invenio-admin v1.0.1). This can be
 # removed when https://github.com/pyupio/safety-db/pull/2274 is merged and
 # released.
-pipenv check --ignore 36759 && \
+
+# Ignoring 36810 (insecure numpy version). This can be removed when Travis
+# updates numpy (https://travis-ci.community/t/issue-pipenv-check-fails-due-to-numpy/2120/3).
+pipenv check --ignore 36759 --ignore 36810 && \
 pipenv run pydocstyle cds_books tests docs && \
 pipenv run isort -rc -c -df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
