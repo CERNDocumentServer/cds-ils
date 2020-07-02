730008081fb277577eaea228358b8eef685f074e
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-07-02 09:11:13 +0200

    tests: ignore latest pipenv version

--- run-tests.sh
+++ run-tests.sh
@@ -12,7 +12,7 @@
 
 # Ignoring 36810 (insecure numpy version). This can be removed when Travis
 # updates numpy (https://travis-ci.community/t/issue-pipenv-check-fails-due-to-numpy/2120/3).
-pipenv check --ignore 36759 --ignore 36810 && \
+pipenv check --ignore 36759 --ignore 36810 --ignore 38334 && \
 pipenv run pydocstyle cds_books tests docs && \
 pipenv run isort -rc -c -df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
