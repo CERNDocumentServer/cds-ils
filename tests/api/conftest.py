37ae0561e0e468ae530dc402891f7b93457e5873
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-07-02 10:42:06 +0200

    tests: whitelist schemas
    * update python packages

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -15,11 +15,11 @@ import os
 import pytest
 from invenio_app.factory import create_api
 from invenio_app_ils.documents.api import DOCUMENT_PID_TYPE, Document
-from invenio_app_ils.internal_locations.api import INTERNAL_LOCATION_PID_TYPE, \
-    InternalLocation
-from invenio_app_ils.items.api import ITEM_PID_TYPE, Item
-from invenio_app_ils.locations.api import LOCATION_PID_TYPE, Location
-from invenio_app_ils.series.api import SERIES_PID_TYPE, Series
+from invenio_app_ils.locations.api import Location, LOCATION_PID_TYPE
+from invenio_app_ils.internal_locations.api import InternalLocation, \
+    INTERNAL_LOCATION_PID_TYPE
+from invenio_app_ils.items.api import Item, ITEM_PID_TYPE
+from invenio_app_ils.series.api import Series, SERIES_PID_TYPE
 from invenio_circulation.api import Loan
 from invenio_circulation.pidstore.pids import CIRCULATION_LOAN_PID_TYPE
 from invenio_db import db
@@ -56,11 +56,16 @@ def app_config(app_config):
         "acquisition",
         "document_requests",
         "documents",
+        "eitems",
         "ill",
-        "ils",
+        "internal_locations",
+        "items",
         "invenio_opendefinition",
         "invenio_records_files",
         "loans",
+        "locations",
+        "series",
+        "vocabularies",
     ]
     return app_config
 
