d1e354a2e87f30d1bd748817d812f419f11793d1
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-07-02 11:39:13 +0200

    tests: pep8

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -15,7 +15,8 @@ import os
 import pytest
 from invenio_app.factory import create_api
 from invenio_app_ils.documents.api import DOCUMENT_PID_TYPE, Document
-from invenio_app_ils.internal_locations.api import INTERNAL_LOCATION_PID_TYPE, \
+from invenio_app_ils.internal_locations.api import \
+    INTERNAL_LOCATION_PID_TYPE, \
     InternalLocation
 from invenio_app_ils.items.api import ITEM_PID_TYPE, Item
 from invenio_app_ils.locations.api import LOCATION_PID_TYPE, Location
