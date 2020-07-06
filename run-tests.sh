47f2d1dad0425cfd50752683884b7905d02be287
Nicola Tarocco <ntarocco@gmail.com>
2020-07-06 13:24:28 +0200

    release: v1.0.0a5

--- run-tests.sh
+++ run-tests.sh
@@ -16,7 +16,7 @@
 export PIPENV_PYUP_API_KEY=""
 pipenv check --ignore 36759 --ignore 36810 --ignore 38334 && \
 pipenv run pydocstyle cds_books tests docs && \
-pipenv run isort -rc -c -df && \
+pipenv run isort -rc -c --df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
 pipenv run sphinx-build -qnNW docs docs/_build/html && \
 pipenv run test
