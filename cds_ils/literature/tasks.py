c00e4dd1828ac9f4b21224cb2854b3d667d64ed1
KonstantinaStoikou <konstantinastoikou1@gmail.com>
2020-09-09 13:24:57 +0200

    ui: fix placeholder bug

--- cds_ils/literature/tasks.py
+++ cds_ils/literature/tasks.py
@@ -10,10 +10,16 @@
 import urllib
 
 from celery import shared_task
+from invenio_app_ils.documents.api import Document
+from invenio_app_ils.documents.indexer import DocumentIndexer
+from invenio_app_ils.series.api import Series
+from invenio_app_ils.series.indexer import SeriesIndexer
 from invenio_db import db
+from invenio_indexer.api import RecordIndexer
+from invenio_search import current_search
 
 from .covers import build_syndetic_cover_urls, has_already_cover, \
-    is_record_with_cover
+    should_record_have_cover
 
 MIN_CONTENT_LENGTH = 128
 
@@ -21,24 +27,30 @@ MIN_CONTENT_LENGTH = 128
 def pick_identifier_with_cover(sender, *args, **kwargs):
     """Triggers async task to set cover metadata of the record."""
     record = kwargs.get("record", {})
-    if is_record_with_cover(record):
+    if should_record_have_cover(record):
         pick_identifier_with_cover_task.apply_async((record,))
 
 
 def is_valid_cover(cover_metadata):
-    """Fetch a cover from url and evaluate if it is valid."""
+    """Fetch all sizes of cover from url and evaluate if they are valid."""
     partial_record = {"cover_metadata": cover_metadata}
-    url = build_syndetic_cover_urls(partial_record)['small']
-    try:
-        resp = urllib.request.urlopen(url)
-        is_success = resp.getcode() == 200
-        more_than_1_pixel = resp.get("Content-Length", 0) > MIN_CONTENT_LENGTH
-    except Exception:
-        return None
+    urls = build_syndetic_cover_urls(partial_record)
+    if urls is None:
+        return False
 
-    if is_success and more_than_1_pixel:
-        return resp
-    return None
+    try:
+        for size in ["small", "medium", "large"]:
+            resp = urllib.request.urlopen(urls[size])
+            has_error = resp.getcode() != 200
+            less_than_1_pixel = (
+                int(resp.getheader("Content-Length")) <= MIN_CONTENT_LENGTH
+            )
+            if has_error or less_than_1_pixel:
+                return False
+    except Exception:
+        return False
+
+    return True
 
 
 def create_identifiers_lists(identifiers):
@@ -58,25 +70,33 @@ def create_identifiers_lists(identifiers):
 
 def save_record(record, cover_metadata):
     """Updates and saves record to the db."""
+    schema = record.get("$schema")
+    if schema.endswith("series-v1.0.0.json"):
+        record = Series.get_record_by_pid(record["pid"])
+        indexer = SeriesIndexer()
+    else:
+        record = Document.get_record_by_pid(record["pid"])
+        indexer = DocumentIndexer()
+
     record["cover_metadata"] = cover_metadata
     record.commit()
     db.session.commit()
+    indexer.index(record)
 
 
 @shared_task(ignore_result=True)
 def pick_identifier_with_cover_task(record):
-    """Set record cover metadata ISBN with valid cover."""
-    if not is_record_with_cover(record):
+    """Set an valid cover identifier to cover metadata, else an empty dict."""
+    if not should_record_have_cover(record):
         return
 
-    if has_already_cover(record) and is_valid_cover(record):
+    if has_already_cover(record) and is_valid_cover(record["cover_metadata"]):
         return
 
     identifiers = record.get("identifiers", [])
     if not identifiers:
         return
 
-    record.setdefault("cover_metadata", {})
     issn_list, isbn_list = create_identifiers_lists(identifiers)
 
     for value in issn_list:
@@ -90,3 +110,17 @@ def pick_identifier_with_cover_task(record):
         if is_valid_cover(cover_metadata):
             save_record(record, cover_metadata)
             return
+
+    # Check if identifier exists in metadata but is invalid (and no other valid
+    # identifier was found).
+    # The function will never go inside this if condition if an update signal
+    # was fired because of `save_record` since the latter will commit in the
+    # cover_metadata either an empty dict {} or a valid identifier (so the
+    # function will have already returned in the above ifs).
+    # The only reason the function will reach inside this is either if a
+    # creation of a record just happened and `preemtively..` function added an
+    # invalid identifier to the cover_metadata, or if an update happened to the
+    # record by a user (not by `save_record`) and the previous identifier was
+    # invalid/non-existent and the updated one is invalid or it was deleted.
+    if record["cover_metadata"] != {}:
+        save_record(record, {})
--- tests/api/test_literature_covers.py
+++ tests/api/test_literature_covers.py
@@ -10,8 +10,8 @@
 from mock import Mock
 
 from cds_ils.literature import tasks
-from cds_ils.literature.covers import build_syndetic_cover_urls, \
-    is_record_with_cover, preemptively_set_first_isbn_as_cover
+from cds_ils.literature.covers import build_cover_urls, \
+    preemptively_set_first_isbn_as_cover, should_record_have_cover
 from cds_ils.literature.tasks import pick_identifier_with_cover
 
 tasks.is_valid_cover = Mock(return_value=True)
@@ -22,35 +22,35 @@ def test_cover_url_builder(app, testdata):
     """Test builder for cover urls."""
     doc = testdata["documents"][0]
     isbn = doc["cover_metadata"]["ISBN"]
-    cover_urls = build_syndetic_cover_urls(doc)
+    cover_urls = build_cover_urls(doc)
     assert cover_urls["small"].endswith("&ISBN={}/SC.gif".format(isbn))
     assert cover_urls["medium"].endswith("&ISBN={}/MC.gif".format(isbn))
     assert cover_urls["large"].endswith("&ISBN={}/LC.gif".format(isbn))
 
     serie = testdata["series"][0]
     issn = serie["cover_metadata"]["ISSN"]
-    cover_urls = build_syndetic_cover_urls(serie)
+    cover_urls = build_cover_urls(serie)
     assert cover_urls["small"].endswith("&ISSN={}/SC.gif".format(issn))
     assert cover_urls["medium"].endswith("&ISSN={}/MC.gif".format(issn))
     assert cover_urls["large"].endswith("&ISSN={}/LC.gif".format(issn))
 
     doc_without_cover = testdata["documents"][1]
-    cover_urls = build_syndetic_cover_urls(doc_without_cover)
+    cover_urls = build_cover_urls(doc_without_cover)
     assert cover_urls["small"].endswith("placeholder.png")
     assert cover_urls["medium"].endswith("placeholder.png")
     assert cover_urls["large"].endswith("placeholder.png")
 
 
-def test_is_record_with_cover(app, testdata):
-    """Test is_record_with_cover helper function."""
+def test_should_record_have_cover(app, testdata):
+    """Test should_record_have_cover helper function."""
     doc = testdata["documents"][0]
-    assert is_record_with_cover(doc)
+    assert should_record_have_cover(doc)
 
     series = testdata["series"][0]
-    assert is_record_with_cover(series)
+    assert should_record_have_cover(series)
 
     item = testdata["items"][0]
-    assert not is_record_with_cover(item)
+    assert not should_record_have_cover(item)
 
 
 def test_preemptively_set_first_isbn_as_cover(app, testdata):
