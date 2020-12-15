c8f29d27141cb2dfe666117bfd4b148e8b89fb90
Konstantina Stoikou <konstantinastoikou1@gmail.com>
2020-12-15 10:47:10 +0100

    Routes: fix not-found route bug

--- ui/src/overridden/routes/ImporterRoute.js
+++ ui/src/overridden/routes/ImporterRoute.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import { Importer } from '../backoffice/Importer';
 import { CdsBackOfficeRoutes } from './BackofficeUrls';
+import { Switch, Route } from 'react-router-dom';
+import { NotFound } from '@inveniosoftware/react-invenio-app-ils';
 
 export const ImporterRoute = () => {
   return (
-    <Route exact path={CdsBackOfficeRoutes.importer} component={Importer} />
+    <>
+      <Route exact path={CdsBackOfficeRoutes.importer} component={Importer} />
+      <Route>
+        <NotFound />
+      </Route>
+    </>
   );
 };
