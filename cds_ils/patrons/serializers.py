740e561d0be37070dbd8e13650c2c113c8f95395
Nicola Tarocco <ntarocco@gmail.com>
2020-12-16 17:52:45 +0100

    fix loan response payload for patron loans

--- cds_ils/patrons/serializers.py
+++ cds_ils/patrons/serializers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 CERN.
+# Copyright (C) 2019-2020 CERN.
 #
 # invenio-app-ils is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -8,34 +8,28 @@
 """Invenio App ILS Patron Loans Information views."""
 
 from flask import jsonify
-from invenio_app_ils.locations.api import Location
 from invenio_app_ils.proxies import current_app_ils
 
 
-def serialize_on_loan_book_information(loan):
+def serialize_on_loan_literature_info(loan):
     """Serialize loan information."""
-    location = Location.get_record_by_pid(loan["transaction_location_pid"])
     Document = current_app_ils.document_record_cls
     document = Document.get_record_by_pid(loan["document_pid"])
     return dict(
         barcode=loan["item"]["barcode"],
+        start_date=loan["start_date"],
         end_date=loan["end_date"],
-        library=location["name"],
-        location=location["address"],
         title=document["title"],
     )
 
 
-def serialize_loan_request_book_information(loan):
+def serialize_loan_request_literature_info(loan):
     """Serialize loan information."""
-    location = Location.get_record_by_pid(loan["transaction_location_pid"])
     Document = current_app_ils.document_record_cls
     document = Document.get_record_by_pid(loan["document_pid"])
     return dict(
-        request_start_date=loan["start_date"],
-        request_end_date=loan["end_date"],
-        library=location["name"],
-        location=location["address"],
+        request_start_date=loan["request_start_date"],
+        request_end_date=loan["request_expire_date"],
         title=document["title"],
     )
 
@@ -47,21 +41,21 @@ def patron_loans_to_dict(patron_loans):
     :return: dict from patron's loan.
     :rtype: dict
     """
-    books_on_loan_results = patron_loans["active_loans"].hits.hits
-    books_on_loan = [
-        serialize_on_loan_book_information(loan["_source"])
-        for loan in books_on_loan_results
+    literatures_on_loan_results = patron_loans["active_loans"].hits.hits
+    literatures_on_loan = [
+        serialize_on_loan_literature_info(loan["_source"])
+        for loan in literatures_on_loan_results
     ]
 
     loan_requests_results = patron_loans["pending_loans"].hits.hits
     loan_requests = [
-        serialize_loan_request_book_information(loan["_source"])
+        serialize_loan_request_literature_info(loan["_source"])
         for loan in loan_requests_results
     ]
 
     response = dict(
-        books_on_loan=books_on_loan,
-        loan_requests=loan_requests,
+        on_loan=literatures_on_loan,
+        requests=loan_requests,
         person_id=patron_loans.get("person_id", ""),
         department=patron_loans.get("department", ""),
     )
