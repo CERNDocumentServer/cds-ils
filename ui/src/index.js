08258a5ffb1aad31c37662f03cfcfeb13bcc4548
jrcastro2 <jrcastro9515@gmail.com>
2021-03-01 15:16:28 +0100

    IE: default message
    * Adds IE check and unsupported
      browser message
    * closes #24

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,11 @@
-import { history, InvenioILSApp } from '@inveniosoftware/react-invenio-app-ils';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import {
+  history,
+  InvenioILSApp,
+  IEFallback,
+} from '@inveniosoftware/react-invenio-app-ils';
 import React from 'react';
-import 'react-app-polyfill/ie11'; // For IE 11 support
 import ReactDOM from 'react-dom';
 import { OverridableContext } from 'react-overridable';
 import { Router } from 'react-router-dom';
@@ -8,11 +13,18 @@ import 'semantic-ui-less/semantic.less';
 import { config } from './config';
 import { overriddenCmps } from './overridableMapping';
 
-ReactDOM.render(
-  <Router history={history}>
-    <OverridableContext.Provider value={overriddenCmps}>
-      <InvenioILSApp config={config} />
-    </OverridableContext.Provider>
-  </Router>,
-  document.getElementById('app')
-);
+// Checks if browser is IE (unsupported)
+const isIE = !!document.documentMode;
+
+if (!isIE) {
+  ReactDOM.render(
+    <Router history={history}>
+      <OverridableContext.Provider value={overriddenCmps}>
+        <InvenioILSApp config={config} />
+      </OverridableContext.Provider>
+    </Router>,
+    document.getElementById('app')
+  );
+} else {
+  IEFallback();
+}
