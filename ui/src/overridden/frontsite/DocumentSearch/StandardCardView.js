61979fa2b26af2674f4fe2e626b5907c9f072b68
Konstantina <tinasto97@gmail.com>
2021-03-24 17:55:05 +0100

    ui: display standard number in search results

---
+++ ui/src/overridden/frontsite/DocumentSearch/StandardCardView.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Label } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+export const StandardCardView = ({ metadata, ...props }) => {
+  const first = metadata.identifiers
+    ? metadata.identifiers
+        .filter(sn => sn.scheme === 'STANDARD_NUMBER')
+        .map(a => a.value)
+        .shift()
+    : null;
+
+  return first ? (
+    <div>
+      <Label
+        basic
+        size="small"
+        color="grey"
+        className="margin-bottom-with-standard-number"
+      >
+        {first}
+      </Label>
+    </div>
+  ) : null;
+};
+
+StandardCardView.propTypes = {
+  metadata: PropTypes.object.isRequired,
+};
