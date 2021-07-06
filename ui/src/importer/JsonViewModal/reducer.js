d4f03d9919d3d3ae20ae5a90962dd229da5ba703
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2021-07-06 17:43:04 +0200

    importer: redux controlled modals

---
+++ ui/src/importer/JsonViewModal/reducer.js
@@ -0,0 +1,24 @@
+import { JSON_MODAL_CLOSE, JSON_MODAL_OPEN } from './actions';
+
+export const initialState = {
+  open: false,
+  title: 'JSON View',
+  json: {},
+};
+
+export const jsonModalViewReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case JSON_MODAL_CLOSE:
+      return { ...state, open: false, title: 'JSON View', json: {} };
+    case JSON_MODAL_OPEN:
+      return {
+        ...state,
+        isLoading: false,
+        title: action.title,
+        json: action.json,
+        open: true,
+      };
+    default:
+      return state;
+  }
+};
---
+++ ui/src/importer/SeriesImportDetailsModal/index.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { openJsonModal } from '../JsonViewModal/actions';
+import { seriesDetailsModalClose, openSeriesDetailsModal } from './actions';
+import SeriesDetailsModalComponent from './SeriesImportDetailsModal';
+
+const mapDispatchToProps = dispatch => ({
+  modalClose: () => dispatch(seriesDetailsModalClose()),
+  modalOpen: seriesReport => dispatch(openSeriesDetailsModal(seriesReport)),
+  jsonModalOpen: (title, json) => dispatch(openJsonModal(title, json)),
+});
+
+const mapStateToProps = state => {
+  return {
+    seriesReport: state.seriesDetailsModal.seriesReport,
+    open: state.seriesDetailsModal.open,
+  };
+};
+
+export const SeriesImportDetailsModal = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SeriesDetailsModalComponent);
