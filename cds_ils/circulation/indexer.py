6c294e5e8e499fe1a41714627f2aa77c8039e53a
pablo <pineirin@gmail.com>
2021-11-25 17:10:06 +0100

    Circulation: add to each loan the first item on shelf * This change only affects elastic search. * The first item is the first of the associated document. * Allow removing fields from the loan serializer. * Closes CERNDocumentServer/cds-ils#499

---
+++ cds_ils/circulation/indexer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Loan indexer APIs."""
+
+from invenio_app_ils.proxies import current_app_ils
+
+
+def index_extra_fields_for_loan(loan_dict):
+    """Indexer hook to modify the loan record dict before indexing.
+
+    The `item_suggestion` fields are added to the loan only on the search index
+    because they are needed for search aggregation/filtering. They are not
+    needed when fetching the loan details.
+    """
+    document_class = current_app_ils.document_record_cls
+    document_record = document_class.get_record_by_pid(
+        loan_dict.get("document_pid")
+    )
+    document = document_record.replace_refs()
+
+    on_shelf = document.get("items", []).get("on_shelf", {})
+
+    if not on_shelf:
+        loan_dict["item_suggestion"] = {}
+        return
+
+    library = next(iter(on_shelf.values()))
+    item_suggestion = next(iter(library.values()))[0]
+
+    for field in [
+        "circulation_restriction",
+        "internal_location_pid",
+        "isbn",
+        "pid",
+    ]:
+        item_suggestion.pop(field, None)
+
+    loan_dict[
+        "item_suggestion"
+    ] = item_suggestion
--- cds_ils/ext.py
+++ cds_ils/ext.py
@@ -8,9 +8,12 @@
 """CDS-ILS extension."""
 
 from flask import Blueprint
+from invenio_indexer.signals import before_record_index
 from invenio_records.signals import after_record_insert, after_record_update
 
 from cds_ils.literature.tasks import pick_identifier_with_cover
+
+from .circulation.indexer import index_extra_fields_for_loan
 
 
 class CdsIls(object):
@@ -20,6 +23,7 @@ class CdsIls(object):
         """Initialization."""
         self.register_signals(app)
         self.init_app(app)
+        self.init_loan_indexer_hook(app)
 
     def init_app(self, app):
         """Initialize app."""
@@ -39,3 +43,26 @@ class CdsIls(object):
         if app.config.get("CDS_ILS_LITERATURE_UPDATE_COVERS", True):
             after_record_insert.connect(pick_identifier_with_cover)
             after_record_update.connect(pick_identifier_with_cover)
+
+    def init_loan_indexer_hook(self, app):
+        """Custom loan indexer hook init."""
+        before_record_index.dynamic_connect(
+            before_loan_index_hook,
+            sender=app,
+            weak=False,
+            index="{0}s-{0}-v1.0.0".format("loan"),
+        )
+
+
+def before_loan_index_hook(
+    sender, json=None, record=None, index=None, **kwargs
+):
+    """Hook to transform loan record before ES indexing.
+
+    :param sender: The entity sending the signal.
+    :param json: The dumped Record dict which will be indexed.
+    :param record: The corresponding Record object.
+    :param index: The index in which the json will be indexed.
+    :param kwargs: Any other parameters.
+    """
+    index_extra_fields_for_loan(json)
