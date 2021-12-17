f1cd2373bc3bb966da317909e6c5fb6548b3110c
pablo <pineirin@gmail.com>
2021-12-17 09:46:42 +0100

    documents: check if document exists when indexing * ensure application handles reindexing records related to a deleted document. * issue appeared when reinxeding a document (and it's records) that was very recently deleted. * add additional comments explaining rationale behind implementation of item_suggestion.

--- cds_ils/circulation/indexer.py
+++ cds_ils/circulation/indexer.py
@@ -8,6 +8,7 @@
 """Loan indexer APIs."""
 
 from invenio_app_ils.proxies import current_app_ils
+from invenio_pidstore.errors import PIDDeletedError
 
 
 def index_extra_fields_for_loan(loan_dict):
@@ -18,17 +19,26 @@ def index_extra_fields_for_loan(loan_dict):
     needed when fetching the loan details.
     """
     document_class = current_app_ils.document_record_cls
-    document_record = document_class.get_record_by_pid(
-        loan_dict.get("document_pid")
-    )
+    try:
+        document_record = document_class.get_record_by_pid(
+            loan_dict["document_pid"]
+        )
+    except PIDDeletedError:
+        # Document might have been deleted while reindexing asynchronously.
+        return
+
     document = document_record.replace_refs()
 
-    on_shelf = document.get("items", []).get("on_shelf", {})
+    on_shelf = document["items"]["on_shelf"]
 
+    # There may be no items on shelf, making impossible to suggest an item.
     if not on_shelf:
         loan_dict["item_suggestion"] = {}
         return
 
+    # Item_suggestion included due to a library request.
+    # The library wanted information about the first item available of each
+    # loan, in order to fasten the management of the pending requests.
     library = next(iter(on_shelf.values()))
     item_suggestion = next(iter(library.values()))[0]
 
