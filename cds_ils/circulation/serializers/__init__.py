43c97eb8caa567eda270fda789520eae946bd849
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-01-13 18:09:02 +0100

    circulation: item suggestion for pending loans

--- cds_ils/circulation/serializers/__init__.py
+++ cds_ils/circulation/serializers/__init__.py
@@ -12,7 +12,32 @@ from invenio_app_ils.records.serializers import record_responsify_no_etag
 from invenio_records_rest.serializers.response import search_responsify
 
 csv_v1 = LoanCSVSerializer(
-    ILSRecordSchemaJSONV1, csv_excluded_fields=["links"]
+    ILSRecordSchemaJSONV1,
+    csv_excluded_fields=[
+        "links",
+        "metadata_$schema",
+        "metadata_document_cover_metadata_ISBN",
+        "metadata_document_cover_metadata_isbn",
+        "metadata_item_description",
+        "metadata_item_document_pid",
+        "metadata_item_medium",
+        "metadata_item_pid_type",
+        "metadata_item_pid_value",
+        "metadata_item_suggestion_status",
+        "metadata_patron_location_pid",
+        "metadata_patron_pid",
+        "metadata_pickup_location_pid",
+        "metadata_pid",
+        "metadata_transaction_user_pid",
+        "metadata_transaction_location_pid",
+        "metadata_transaction_date",
+        "metadata_document_authors",
+        "metadata_document_document_type",
+        "metadata_document_edition",
+        "metadata_document_publication_year",
+        "metadata_patron_id",
+        "metadata_item_suggestion_internal_location_location_name",
+    ]
 )
 csv_v1_response = record_responsify_no_etag(csv_v1, "text/csv")
 csv_v1_search = search_responsify(csv_v1, "text/csv")
