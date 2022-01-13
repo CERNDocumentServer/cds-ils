0049425ad77cdc8eaad3b778296c382085cc7e14
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-01-13 18:09:02 +0100

    circulation: item suggestion for pending loans

--- cds_ils/circulation/indexer.py
+++ cds_ils/circulation/indexer.py
@@ -6,7 +6,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Loan indexer APIs."""
-
+from flask import current_app
 from invenio_app_ils.proxies import current_app_ils
 from invenio_pidstore.errors import PIDDeletedError
 
@@ -19,6 +19,7 @@ def index_extra_fields_for_loan(loan_dict):
     needed when fetching the loan details.
     """
     document_class = current_app_ils.document_record_cls
+    item_suggestion = {}
     try:
         document_record = document_class.get_record_by_pid(
             loan_dict["document_pid"]
@@ -31,16 +32,25 @@ def index_extra_fields_for_loan(loan_dict):
 
     on_shelf = document["items"]["on_shelf"]
 
+    is_request = loan_dict["state"] in \
+        current_app.config["CIRCULATION_STATES_LOAN_REQUEST"]
+
     # There may be no items on shelf, making impossible to suggest an item.
-    if not on_shelf:
-        loan_dict["item_suggestion"] = {}
+    if not on_shelf or not is_request:
+        loan_dict["item_suggestion"] = item_suggestion
         return
 
     # Item_suggestion included due to a library request.
-    # The library wanted information about the first item available of each
-    # loan, in order to fasten the management of the pending requests.
+    # field was added to the loan indexer to avoid performing these operations
+    # during CSV serialization - which would delay the process
     library = next(iter(on_shelf.values()))
-    item_suggestion = next(iter(library.values()))[0]
+    location = next(iter(library.values()))
+
+    for item in location:
+        item_not_on_loan = item.get("circulation") is None
+        item_can_circulate = item["status"] == "CAN_CIRCULATE"
+        if item_not_on_loan and item_can_circulate:
+            item_suggestion = item
 
     for field in [
         "circulation_restriction",
