391475596ee36440cfc2a032ff99b47be16b66cc
Saksham Arora <sakshamarora1001@gmail.com>
2024-04-12 12:46:32 +0200

    overridden: frontsite: DocumentDetails: Integrate MapCERN

--- ui/src/overridden/frontsite/Document/DocumentDetails/DocumentItemBody.js
+++ ui/src/overridden/frontsite/Document/DocumentDetails/DocumentItemBody.js
@@ -1,45 +1,31 @@
 import React from "react";
-import { DocumentItemBody } from "@inveniosoftware/react-invenio-app-ils";
+import { DocumentItemBody, InfoPopup } from "@inveniosoftware/react-invenio-app-ils";
 import _get from "lodash/get";
 import { parametrize } from "react-overridable";
-import { Table } from "semantic-ui-react";
-
-export const DocumentItemTableHeader = () => {
-  return (
-    <Table.Row data-test="header">
-      <Table.HeaderCell>Barcode</Table.HeaderCell>
-      <Table.HeaderCell>Shelf</Table.HeaderCell>
-      <Table.HeaderCell>Call Number</Table.HeaderCell>
-      <Table.HeaderCell>Status</Table.HeaderCell>
-      <Table.HeaderCell>Medium</Table.HeaderCell>
-      <Table.HeaderCell>Loan restriction</Table.HeaderCell>
-    </Table.Row>
-  );
-};
-
-function renderShelflink(item) {
-  const shelfNumber = _get(item, "shelf");
-  if (shelfNumber === undefined) return;
-  const shelfLink = `https://maps.web.cern.ch/?n=['SHELF ${shelfNumber}']`;
-  return (
-    <a href={shelfLink} target="_blank" rel="noreferrer">
-      {shelfNumber}
-    </a>
-  );
-}
+import { shelfLink, shelfLinkComponent } from "../../utils";
 
 function renderCallNumber(item) {
   const identifiers = _get(item, "identifiers", []);
   if (identifiers === null) {
-    return "-";
+    return "Call number: -";
   }
   const callNumber = identifiers.find(
     (identifier) => identifier.scheme.toLowerCase() === "call number"
   );
-  return callNumber ? callNumber.value : "-";
+  return "Call number: " + (callNumber ? callNumber.value : "-");
+}
+
+function renderShelflink(item) {
+  const shelfNumber = _get(item, "shelf");
+  if (shelfNumber === undefined) return;
+  const itemShelfLink = shelfLink(shelfNumber);
+  return (
+    <InfoPopup message={renderCallNumber(item)}>
+      {shelfLinkComponent(itemShelfLink, shelfNumber)}
+    </InfoPopup>
+  );
 }
 
 export const DocumentItemBodyTable = parametrize(DocumentItemBody, {
   shelfLink: renderShelflink,
-  callNumber: renderCallNumber,
 });
