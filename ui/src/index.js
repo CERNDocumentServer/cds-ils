7218079d89044cbd08081ea50786a21c2f42d85d
jakob miesner <jakob.miesner@gmail.com>
2026-02-27 13:57:36 +0100

    ui: update all dependencies

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,6 @@
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-import {
-  history,
-  InvenioILSApp,
-  IEFallback,
-} from "@inveniosoftware/react-invenio-app-ils";
+import { history, InvenioILSApp } from "@inveniosoftware/react-invenio-app-ils";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { OverridableContext } from "react-overridable";
 import { Router } from "react-router-dom";
 import "semantic-ui-less/semantic.less";
@@ -14,19 +8,13 @@ import { initialiseReducers } from "./reducers";
 import { config } from "./config";
 import { overriddenCmps } from "./overridableMapping";
 
-// Checks if browser is IE (unsupported)
-const isIE = !!document.documentMode;
 initialiseReducers();
 
-if (!isIE) {
-  ReactDOM.render(
-    <Router history={history}>
-      <OverridableContext.Provider value={overriddenCmps}>
-        <InvenioILSApp config={config} />
-      </OverridableContext.Provider>
-    </Router>,
-    document.getElementById("app")
-  );
-} else {
-  IEFallback();
-}
+const root = createRoot(document.getElementById("app"));
+root.render(
+  <Router history={history}>
+    <OverridableContext.Provider value={overriddenCmps}>
+      <InvenioILSApp config={config} />
+    </OverridableContext.Provider>
+  </Router>
+);
